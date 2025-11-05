a40c2a41d4b0a8adc30b86ddd7ce8fd7a25448eb
jlemoslemons <wilsonlemos54@gmail.com>
2025-11-04 21:28:35 -0300

    att

--- views/partials/admin/form/header.php
+++
@@ -1,6 +0,0 @@
-<div class="card-header bg-white d-flex justify-content-between align-items-center py-3">
-    <h5 class="mb-0 fw-semibold" id="formTitle"><?= $title ?></h5>
-    <button onclick="history.back()" class="btn btn-secondary" id="btnCancelForm">
-        <i class="bi bi-arrow-left"></i> Voltar
-    </button>
-</div>
